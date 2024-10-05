fc642c2de75f8104a890a126fc35db9cb38f75e4
ffssea <bugducky@outlook.com>
2024-10-05 22:46:54 +0800

    support generic csv to json and yaml

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use rcli::{process_csv, Opts, Subcommand};
 
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
+
     match opts.cmd {
         Subcommand::Csv(opts) => {
-            process_csv(&opts.input, &opts.output)?;
+            let output = if let Some(output) = opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+            process_csv(&opts.input, output, opts.format)?;
         }
     }
     Ok(())
